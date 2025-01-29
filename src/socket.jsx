96ca9b99fcd918738340e6f28f00122aeb65bfbb
dongbinkoo <rnehdqlsdbxbqm@gmail.com>
2025-01-29 19:53:16 +0900

    화면공유 수정

--- src/socket.jsx
+++ src/socket.jsx
@@ -1,8 +1,9 @@
 import { io } from 'socket.io-client';
 
+// WebSocket 우선으로 설정
 const socket = io('https://drawapp-ne15.onrender.com', {
-    transports: ['websocket', 'polling'],
-    reconnection: true,
+    transports: ['websocket', 'polling'], // WebSocket 우선
+    reconnection: true, // 자동 재연결 활성화
     reconnectionAttempts: 10,
     reconnectionDelay: 2000,
 });
