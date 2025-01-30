b262a2d83a8c8716cf109ed94701e50c1076e6b2
dongbinkoo <rnehdqlsdbxbqm@gmail.com>
2025-01-30 17:10:17 +0900

    peer.js 추가 및 ui 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import ScreenShare from './components/ScreenShare';
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<ScreenShare />} />
-                <Route path="*" element={<div>404 Not Found</div>} />
-            </Routes>
-        </Router>
+        <Routes>
+            <Route path="/" element={<ScreenShare />} />
+            <Route path="*" element={<div>404 Not Found</div>} />
+        </Routes>
     );
 }
 
