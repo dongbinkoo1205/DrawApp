1050c5e5875c2ba20bd8e38c70cfb3450a0b053f
dongbinkoo <rnehdqlsdbxbqm@gmail.com>
2025-02-03 14:55:11 +0900

    참여자 캐릭터 설정

--- src/components/CharacterSelector.jsx
+++ src/components/CharacterSelector.jsx
@@ -19,14 +19,14 @@ const CharacterSelector = ({ selectedCharacter, setSelectedCharacter }) => {
                     key={char.id}
                     onClick={() => handleClick(char)}
                     className={`w-1/2 h-full flex transition-all duration-500 justify-center items-center relative ${
-                        selectedCharacter?.id === char.id ? 'bg-white' : 'bg-black'
+                        selectedCharacter?.id === char.id ? 'bg-gradient-to-br from-blue-500 to-purple-800' : 'bg-black'
                     } hover:scale-105 transform`}
                 >
                     <div className="flex flex-col justify-center items-center px-8 text-center">
                         <img
                             src={char.avatar}
                             alt={char.name}
-                            className="w-73 h-73 object-contain mb-4 opacity-90 hover:opacity-100"
+                            className="w-71 h-71 object-contain mb-4 opacity-90 hover:opacity-100"
                         />
                         <h2 className="text-3xl font-extrabold tracking-wide text-white">{char.name}</h2>
                     </div>
--- src/components/NicknameInput.jsx
+++ src/components/NicknameInput.jsx
@@ -15,8 +15,11 @@ const NicknameInput = ({ onSubmit, selectedCharacter, setSelectedCharacter }) =>
 
     return (
         <div className="w-full h-full  absolute top-0 left-0  h-auto z-10 flex items-center justify-center">
-            <div className="flex flex-col items-center justify-around min-h-70 text-white  bg-black w-4/5 h-70p p-15 rounded-lg">
-                <h1 className="text-2xl font-bold animate-pulse">채팅에서 사용할 캐릭터와 이름을 입력해주세요.</h1>
+            <div className="flex flex-col items-center justify-around min-h-70 text-white  bg-black w-[90%] h-70p p-15 rounded-lg">
+                <h1 className="text-2xl font-bold self-baseline">
+                    Participation
+                    <span className="text-small block">채팅에서 사용할 캐릭터와 이름을 입력해주세요.</span>
+                </h1>
                 <CharacterSelector selectedCharacter={selectedCharacter} setSelectedCharacter={setSelectedCharacter} />
                 <div>
                     <input
